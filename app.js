b68822dfa0be71fbf75cc8df4135abcd4adf7d8a
TarasovAlex <tarasof.av@gmail.com>
2023-05-12 10:28:23 +0300

    start learning js

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Hello world!");
+//
+// let a = 5; //number
+// a = "hello"; //string
+// a = false; //boolean
+let a  = 5 + "5";
+console.log(a, typeof a);
+a = a - 5;
+console.log(a, typeof a);
+a = "abc";
+a /= 2;
+console.log(a, typeof a);
