f8b9f56fabfcfa0a1e8412fc15407cb3959fca9c
TarasovAlex <tarasof.av@gmail.com>
2023-05-13 22:21:51 +0300

    primitives

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-console.log("Hello world!");
-//
+// console.log("Hello world!");
 // let a = 5; //number
 // a = "hello"; //string
 // a = false; //boolean
-let a  = 5 + "5";
-console.log(a, typeof a);
-a = a - 5;
-console.log(a, typeof a);
-a = "abc";
-a /= 2;
-console.log(a, typeof a);
+// let a  = 5 + "5";
+// console.log(a, typeof a);
+// a = a - 5;
+// console.log(a, typeof a);
+// a = "abc";
+// a /= 2;
+// console.log(a, typeof a);
+// a = "123";
+// // a -= 0;
+// a = +a;
+// console.log(a, typeof a);
+// if(a = 1){
+//     console.log("a is true");
+// }
+// console.log(a, typeof a);
+// let a = 1;
+// let b = 3;
+// let c = 2;
+// if(a < b < c){
+//     console.log("a < b < c is true")
+// }
+// let a = new Number(3);
+// console.log(a + 5, typeof a)
+// let a = 0.3456789;
+// console.log(a, typeof a);
+// a = a.toFixed(2);
+// console.log(a, typeof a);
+
+// Math.trunc, Math.round, 
+
+// console.log(("A" + +"A" + "AS").toLowerCase());      // как написать слово ananas*
+
+function myToStringInt(number, radix){   
+
+}
