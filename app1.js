50515f0ed24fc37c9df9b3158b449768b270a839
tarrasoff <88909508+tarrasoff@users.noreply.github.com>
2023-05-17 11:11:18 +0300

    json

--- app1.js
+++ app1.js
@@ -1,7 +1,4 @@
-function displayOccurrences(array) {
-    const occurrences = array.reduce((obj, s) => ({...obj, [s] : obj[s] ? obj[s] + 1 : 1}), {});
-    Object.entries(occurrences).sort((e1, e2) => e1[1] == e2[1] ? e1[0].localeCompare(e2[0]) : e2[1] - e1[1])
-    .forEach(e => console.log(`${e[0]} -> ${e[1]}`))
-}
-
-displayOccurrences(["lmn", "ab", "lmn", "c", "d", "ab", "a", "a", "lmn"]);
+console.log(JSON.stringify(rectangle)[0]);
+//самая глубокая копия объета, строки
+const rectangle = JSON.parse(JSON.stringify(rectangle1)); 
+const rectangle3 = {...rectangle1};
