e176f1031222d3ebb0b86bcf1f9575646790dd38
tarrasoff <88909508+tarrasoff@users.noreply.github.com>
2023-05-17 12:48:19 +0300

    oop-js

---
+++ app.js
@@ -0,0 +1,30 @@
+class Rectangle {
+    #width;
+    #height;
+    constructor(width, height) {
+        this.#width = width;
+        this.#height = height;
+    }
+    square(){
+        return this.#width * this.#height;
+    }
+    perimeter(){
+        return 2 * (this.#width + this.#height);
+    }
+}
+const rectangle = new Rectangle(3, 4);
+console.log(rectangle.square());
+let c;
+class Square extends Rectangle{
+    constructor(width){
+        super(width, width);
+    }
+}
+const square = new Square(10);
+console.log(square.perimeter());
+Array.prototype.map = function(){
+    console.log(this);
+    return 'kuku'
+};
+const ar = [1, 2, 3];
+console.log(ar.map());
