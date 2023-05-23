0a23239e34086816aea1b081d36c564cd41ed4f9
tarrasoff <88909508+tarrasoff@users.noreply.github.com>
2023-05-24 00:07:00 +0300

    code of getTemperatures

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
-//what's wrong
-function sleep(timeout) {
-    return new Promise(resolve => setTimeout(() => resolve(), timeout));
-}
-function f1() {
-    console.log('f1 performed');
-}
-function f2() {
-    console.log('f2 performed');
-}
-function f3() {
-    console.log('f3 performed');
-}
-// const promise = sleep(2000);
-// promise.then(() => f1()).then(() => f2()).then(() => f3());
-
-function getId(predicate) {
-    const ids = [123, 124, 125, 126];
-    const index = ids.findIndex(predicate);
-    return new Promise((resolve, reject) => {
-        return index < 0 ? reject('id not found') : resolve(ids[index])
-    }, 1000);
-}
-function getCar(id) {
-    const cars = {
-        '123': 'suzuki',
-        '124': 'hunday',
-        '125': 'honda'
-    }
-    const car = cars[id];
-    return new Promise((resolve, reject) =>
-        setTimeout(() => car ? resolve(car) : reject('no car found'), 1000))
-}
-// function displayCar(predicate){
-//     getId(predicate).then(id => getCar(id)).then(car => console.log(car)).catch(error => console.log(error))
+// async function getTemperatures(lat, long){
+//     const response = 
+//     await fetch(`https://api.open-meteo.com/v1/gfs?latitude=${lat}&longitude=${long}&hourly=temperature_2m,apparent_temperature&timezone=IST`);
+//     return response.json();
 // }
+// getTemperatures(31.25, 34.79).then((data) => console.log(data))
+// .catch(error => console.log(error))
 
-async function displayCar(predicate) {
-    await sleep(2000);
-    try {
-        const id = await getId(predicate)
-        const car = await getCar(id);
-        console.log(car);
-    } catch (error) {
-        console.log(error)
-    }
+async function getTemperatures(lat, long, startDate, days, hourFrom, hourTo){
+    //Parameters: lat - latitude, long - longitude, startDate - ISO date of forecast,
+    // days - forecast days, hourFrom, hourTo - closed range of the hours for each day
+    // returns:
+    // array of objects {date: <string containing date YYYY-MM-YY with no time>,
+    // time: <hour number from the given range>, temperature: <number>,
+    // apperantTemperature: <number> }
+    const endDate = getEndDate(startDate, days);
+    const url = getUrl(lat, long, startDate, endDate);
+    const response = await fetch(url);
+    const data = response.json();
+    const dates = getDataForHours(data.hourly.time, hourFrom, hourTo);
+    const temperatures = getDataForHours(data.hourly.temperature_2m, hourFrom, hourTo);
+    const apparentTemperatures = getDataForHours(data.hourly.apparent_temperature, hourFrom, hourTo);
+    return dates.map((d, index) => {
+        const tokens = d.split("T");
+        const date = tokens[0];
+        const time = tokens[1];
+        return {date, time, temperature: temperatures[index],
+             apparentTemperature: apparentTemperatures[index]};
+    })
 }
-displayCar(id => id == 126).then(() => console.log('thanks & good bye'));
-console.log('waiting for the data ...');
 
+function getEndDate(startDateStr, days){
+    const startDate = new Date(startDateStr);
+    const endDate = new Date(startDate.setDate(startDate.getDate() + days));
+    return endDate.toISOString().substring(0, 10);
+}
+
+function getUrl(lat, long, startDate, endDate){
+    return `https://api.open-meteo.com/v1/gfs?latitude=${lat}&longitude=${long}&hourly=temperature_2m,apparent_temperature&timezone=IST&start_date=${startDate}&end_date=${endDate}`
+}
+
+function getDataForHours(array, hourFrom, hourTo){
+    return array.filter((__, index) => {
+        const rem = index % 24;
+        return rem >= hourFrom && rem <= hourTo
+    })
+}
+
+getTemperatures(31.25, 34.79, 2023-05-23, 2, 12,  14).then((data) => console.log(data))
+.catch(error => console.log(error))
