65a030f6e40e118ef8835e591535981e5d3e5bbe
tarrasoff <88909508+tarrasoff@users.noreply.github.com>
2023-05-23 17:19:32 +0300

    Promise introdaction

--- app.js
+++ app.js
@@ -1,86 +1,36 @@
-// write myForEach to call from any array with the same behavioral as the standard forEach
-// write myMap to call from any array
-// write myFilter
-// write myReduce
-
-Array.prototype.myForEach = function (func) {
-    for (let i = 0; i < this.length; i++) {
-        func(this[i], i, this);
-    }
+//what's wrong
+function sleep(timeout) {
+    return new Promise(resolve => setTimeout(() => resolve(), timeout));
 }
-const array = [1, 2, 3, 4, 5];
-
-array.myForEach(n => console.log(n));
-
-Array.prototype.myMap = function (func) {
-    let newArray = [];
-    for (let i = 0; i < this.length; i++) {
-        newArray[i] = func(this[i], i, this);
-    }
-    return newArray;
+function f1() {
+    console.log('f1 performed');
 }
-
-console.log(array.myMap(n => n + 1));
-
-Array.prototype.myFilter = function (func) {
-    let res = [];
-    for (let i = 0; i < this.length; i++) {
-        if (func(this[i], i, this)) {
-            res.push(this[i]);
-        }
-    }
-    return res;
+function f2() {
+    console.log('f2 performed');
 }
-
-console.log(array.myFilter(n => n > 3));
-
-Array.prototype.myReduce = function (func, value) {
-    let res = value;
-    let i = 0;
-    if (value == undefined) {
-        res = this[0];
-        i = 1;
-    }
-    if (res != undefined) {
-        for (i; i < this.length; i++) {
-            res = func(res, this[i], i, this);
-        }
-    }
-    return res;
+function f3() {
+    console.log('f3 performed');
 }
+// const promise = sleep(2000);
+// promise.then(() => f1()).then(() => f2()).then(() => f3());
 
-console.log(array.myReduce(((a, b) => a + b), 10));
-
-class Deferred {
-    ar = [];
-    then(func) {
-        this.ar.push(func)
-    }
-
-    resolve(e1) {
-        this.ar.forEach(func => e1 = func(e1));
-    }
+function getId(predicate) {
+    const ids = [123, 124, 125];
+    const index = ids.findIndex(predicate);
+    return new Promise((resolve, reject) => {
+        return index < 0 ? reject('id not found') : resolve(ids[index])
+    }, 1000);
 }
-
-const d = new Deferred()
-
-d.then(function (res) {
-    console.log("1 ", res);
-    return "a";
-});
-
-d.then(function (res) {
-    console.log("2 ", res);
-    return "b";
-});
-
-d.then(function (res) {
-    console.log("3 ", res);
-    return "c";
-});
-
-d.resolve('hello');
-
-// 1  hello
-// 2  a
-// 3  b
+function getCar(id) {
+    const cars = {
+        '123': 'suzuki',
+        '124': 'hunday',
+        '125': 'honda'
+    }
+    const car = cars[id];
+    return new Promise((resolve, reject) => setTimeout(() => car ? resolve(car) : reject('no car found'), 1000))
+}
+function displayCar(predicate){
+    getId(predicate).then(id => getCar(id)).then(car => console.log(car)).catch(error => console.log(error))
+}
+displayCar(id => id == 125);
