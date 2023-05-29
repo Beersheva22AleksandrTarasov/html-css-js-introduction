9f005f17815458ffcd5ceafe1bdcd45fd190f103
tarrasoff <88909508+tarrasoff@users.noreply.github.com>
2023-05-29 13:07:18 +0300

    employees application

--- src/ui/DataGrid.js
+++ src/ui/DataGrid.js
@@ -2,31 +2,33 @@ export default class DataGrid {
     #tBodyElement
     #keys
     constructor(parentId, columns) {
-        // columns - array of objects (field: <name of key>,
-        // headerName: <column name>)
+        //columns - array of objects {field: <name of key>,
+        // headerName: <column name>}
         this.#keys = columns.map(c => c.field);
-        this.buildTableHeader(parentId, columns.map(c => c.headerName))
+        this.#buildTableHeader(parentId, columns.map(c => c.headerName))
+
     }
     fillData(rowsData) {
         this.#tBodyElement.innerHTML = rowsData.map(rd => this.#getRow(rd)).join('');
     }
     #getRow(obj) {
         return `<tr>
-                   ${this.#keys.map(key => `<td>${obj[key]}</td>`).join('')}
-                 </tr>`
+                   ${this.#keys.map(key => `<td>${obj[key]}</td>` ).join('')}
+                 </tr>  `
     }
-    buildTableHeader(parentId, columnNames) {
+    #buildTableHeader(parentId, columnNames) {
         const tableSectionElement = document.getElementById(parentId);
         tableSectionElement.innerHTML =
             `<table>
-            <theader>
-                <tr>
-                    ${columnNames.map(headerName => `<th>${headerName}</th>`).join('')}
-                </tr>
+            <thead>
+               <tr>
+                   ${columnNames.map(headerName => `<th>${headerName}</th>`).join('')}
+               </tr>
             </thead>
-            <tbody id="${parentId}-table">
+            <tbody id="${parentId}-table" >
             </tbody>
-        </table>`
+          </table>`
         this.#tBodyElement = document.getElementById(parentId + "-table")
+
     }
 }
